8c81afec90f267e19ef4cbf058cc0a307f1d10d4
Avishka Indula <avishkainduladev@gmail.com>
2023-05-29 07:22:18 +0530

    used double quotes instead of single quotes

--- moderation/index.js
+++ moderation/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', async (req, res) => {
+app.post("/events", async (req, res) => {
   console.log(req.body);
   const { type, data } = req.body;
 
-  if (type === 'CommentCreated') {
-    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
 
-    await axios.post('http://localhost:4005/events', {
-      type: 'CommentModerated',
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
       data: {
         id: data.id,
         postId: data.postId,
@@ -27,5 +27,5 @@ app.post('/events', async (req, res) => {
 });
 
 app.listen(4003, () => {
-  console.log('listening on port 4003');
+  console.log("listening on port 4003");
 });
